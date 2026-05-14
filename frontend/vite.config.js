5a91e1b381737a8f4137194df7a59d51015ea9e3
Kartikey-Tiwari-971 <157580503+Kartikey-Tiwari-971@users.noreply.github.com>
2026-05-14 20:14:38 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
+  build: {
+    outDir: 'dist',  
   },
+  
 })
