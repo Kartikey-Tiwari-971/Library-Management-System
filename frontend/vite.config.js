a26eb7fcabe11079e5040ff3d246a53fac8e9371
Kartikey-Tiwari-971 <157580503+Kartikey-Tiwari-971@users.noreply.github.com>
2026-05-14 20:42:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist',  
+    outDir: resolve(__dirname, '../dist'),
+    emptyOutDir: true,
   },
-  
 })
